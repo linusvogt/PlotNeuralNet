2211b3f1c03c6bd08fc8a3af81e8f021cd6cc668
Linus Vogt <linus.vogt@gmail.com>
2026-03-24 15:10:52 -0400

    First draft of unet

--- diffusion_unet/unet.py
+++ diffusion_unet/unet.py
@@ -3,20 +3,87 @@ sys.path.append('../')
 from pycore.tikzeng import *
 
 unet = [
-    ### ----- ENCODER ----- ###
     to_head( '..' ),    # preamble
     to_cor(),           # define colors
     to_begin(),         # begin document
 
     ### -- Input
-    to_input()
-    # to_Conv("conv1", 512, 64, offset="(0,0,0)", to="(0,0,0)", height=64, depth=64, width=2 ),
-    # to_Pool("pool1", offset="(0,0,0)", to="(conv1-east)"),
-    # to_Conv("conv2", 128, 64, offset="(1,0,0)", to="(pool1-east)", height=32, depth=32, width=2 ),
-    # to_connection( "pool1", "conv2"),
-    # to_Pool("pool2", offset="(0,0,0)", to="(conv2-east)", height=28, depth=28, width=1),
-    # to_SoftMax("soft1", 10 ,"(3,0,0)", "(pool1-east)", caption="SOFT"  ),
-    # to_connection("pool2", "soft1"),
+    to_input("/home/linus/PlotNeuralNet/diffusion_unet/input.png", name='input'),
+    
+    ### -- E1; note: D=(H, W)64
+    to_ConvConvRelu(
+        name="e1",
+        s_filer='D', n_filer=(32, 32),
+        offset="(1,0,0)", to="(0,0,0)",
+        height=25, depth=50, width=(2, 2),
+        caption='E1'
+    ),
+
+    ### -- E2
+    to_ConvConvRelu(
+        name="e2",
+        s_filer='D/2', n_filer=(64, 64),
+        offset="(2,-4,0)", to="(e1-east)",
+        height=25, depth=50, width=(4, 4),
+        caption='E2'
+    ),
+    to_connection("e1", "e2"),
+
+    ### -- E3
+    to_ConvConvRelu(
+        name="e3",
+        s_filer='D/4', n_filer=(128, 128),
+        offset="(2,-4,0)", to="(e2-east)",
+        height=25, depth=50, width=(8, 8),
+        caption='E3'
+    ),
+    to_connection("e2", "e3"),
+
+    ### -- Bottleneck
+    to_ConvConvRelu(
+        name="bottleneck",
+        s_filer='D/4', n_filer=(128, 128),
+        offset="(5,0,0)", to="(e3-east)",
+        height=25, depth=50, width=(8, 8),
+        caption='Bottleneck'
+    ),
+    to_connection("e3", "bottleneck"),
+
+    ### -- D3
+    to_ConvConvRelu(
+        name="d3",
+        s_filer='D/4', n_filer=(128, 128),
+        offset="(5,0,0)", to="(bottleneck-east)",
+        height=25, depth=50, width=(8, 8),
+        caption='D3'
+    ),
+    to_connection("bottleneck", "d3"),
+
+    ### -- D2
+    to_ConvConvRelu(
+        name="d2",
+        s_filer='D/2', n_filer=(64, 64),
+        offset="(5,4,0)", to="(d3-east)",
+        height=25, depth=50, width=(4, 4),
+        caption='D2'
+    ),
+    to_connection("d3", "d2"),
+
+    ### -- D1
+    to_ConvConvRelu(
+        name="d1",
+        s_filer='D', n_filer=(32, 32),
+        offset="(5,4,0)", to="(d2-east)",
+        height=25, depth=50, width=(2, 2),
+        caption='D1'
+    ),
+    to_connection("d2", "d1"),
+
+    to_skip("e1", "d1"),
+    to_skip("e2", "d2"),
+    to_skip("e3", "d3"),
+
+
     to_end()
 ]
 
